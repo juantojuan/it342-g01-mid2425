b84c3f33f2dba5b4b997922f8c04e7c35a519dd1
LAB12_USER ON WS21 <L12X15W21@citulab.net>
2025-06-16 18:04:14 +0800

    OAuth implementation

---
+++ derecho-napadriel-journal-activity/oauth2login/src/main/java/com/derecho/oauth2login/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.derecho.oauth2login.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests.anyRequest().authenticated())
+                .oauth2Login(oauth2 -> oauth2.defaultSuccessUrl("http://localhost:8080/user-info", true))
+                .logout(logout -> logout.logoutSuccessUrl("/"))
+                .formLogin(form -> form.defaultSuccessUrl("/secured", true))
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}
